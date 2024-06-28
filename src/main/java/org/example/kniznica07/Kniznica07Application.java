7b94fcea34ffb348c5e7110c85a018f66b002c81
77satyr77 <77satyr77@gmail.com>
2024-06-28 13:17:24 +0200

    prihlasovacia obrazovka

--- src/main/java/org/example/kniznica07/Kniznica07Application.java
+++ src/main/java/org/example/kniznica07/Kniznica07Application.java
@@ -1,13 +1,17 @@
 package org.example.kniznica07;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class Kniznica07Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Kniznica07Application.class, args);
+
+        log.info("http://localhost:8081/");
     }
 
 }
--- src/main/java/org/example/kniznica07/controller/MvcController.java
+++ src/main/java/org/example/kniznica07/controller/MvcController.java
@@ -2,12 +2,14 @@ package org.example.kniznica07.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MvcController {
 
     @GetMapping
-    public String nacitajIndex(){
+    public String nacitajPrihlasovaciuObrazovku(){
         return "index";
     }
 }
